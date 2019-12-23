912213be7a13e3dfd861fb0f636db753092930a6
Lukyanov Artyom <artglz63@gmail.com>
2019-12-24 02:48:06 +0300

    food fix

--- controllers/user/User/Food.js
+++ controllers/user/User/Food.js
@@ -2,7 +2,7 @@ const { utils, db } = require('../../../services/')
 
 const Food = {
   async setCurrentFoodMenu(userId) {
-    const menuId = 1
+    const menuId = 0
     // TODO: расчет id плана питания
     const query = `UPDATE users SET food = jsonb_set(food,'{menu_id}'::text[], $1) WHERE id=$2`
     const values = [menuId, userId]

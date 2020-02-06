58ffc1bd9cdc677a5dc4c32e39499e6e3565c5f1
Lukyanov Artyom <artglz63@gmail.com>
2020-02-06 22:33:48 +0300

    workouts almost done + fixes

--- controllers/user/Workout.js
+++ controllers/user/Workout.js
@@ -3,11 +3,12 @@ const { DATA } = require('../../data')
 
 const Workout = {
   async get(req, res) {
-    const query = `SELECT workout, EXTRACT('DAY' FROM (current_timestamp - (workout->>'start_date')::timestamp::date)) as days_from_start FROM users WHERE id=$1`
+    const query = `SELECT workout, EXTRACT('DAY' FROM (current_timestamp - (workout->>'start_date')::timestamp::date)) as days_from_start, extract(dow from current_timestamp) as _dow FROM users WHERE id=$1`
     const values = [req.currentUser.id]
     try {
       const { rows } = await db.query(query, values)
-
+      const dow = rows[0]._dow
+      //if ([1, 3, 5].includes(dow)) {
       const _workout = rows[0].workout
       const days_from_start = rows[0].days_from_start
       if (_workout) {
@@ -15,15 +16,17 @@ const Workout = {
           DATA.workouts.relation[_workout.physical_level][
             _workout.overweight_level
           ]
-        let workout = {}
+        let workout = {
+          day_number: days_from_start + 1
+        }
         if (typeof workout_ids.gym === 'number') {
           const wg = DATA.workouts.plans.filter(
             item => item.id === workout_ids.gym
           )[0]
           workout.gym = {}
-          //workout.gym.id = wg.id
           workout.gym.description = wg.description
-          workout.gym.exercises = wg.days[days_from_start]
+          // TODO: add proper days offset here
+          workout.gym.exercises = wg.days[days_from_start].exercises
         } else workout.gym = null
 
         if (typeof workout_ids.home === 'number') {
@@ -32,13 +35,13 @@ const Workout = {
           )[0]
           workout.home = {}
 
-          //workout.home.id = wh.id
           workout.home.description = wh.description
-          workout.home.exercises = wh.days[days_from_start]
+          // TODO: add proper days offset here
+          workout.home.exercises = wh.days[days_from_start].exercises
         } else workout.home = null
-        console.log(workout)
         return utils.response.success(res, workout)
-      }
+      } else return utils.response.success(res, { day_off: true })
+      //}
     } catch (e) {
       console.log(e)
       return utils.response.error(res)
